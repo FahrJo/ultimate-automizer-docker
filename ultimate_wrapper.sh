6563c4a3a15bb6b175338c41fada723194ddb016
FahrJo <johannes.fahr@posteo.de>
2023-04-25 14:35:41 +0200

    check for files only if necessary

--- ultimate_wrapper.sh
+++ ultimate_wrapper.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-otherArguments=""
-
 nextArgIsToolchain=false
 nextArgIsInput=false
 nextArgIsSetting=false
 nextArgIsCsvDir=false
+
+mkdir -p ./logfiles
+mountArguments="--mount type=bind,source=$(pwd)/logfiles,target=/home/logfiles "
+otherArguments=""
 
 # make file path absolute
 absolutePath () {
@@ -40,50 +42,45 @@ do
       if test "$nextArgIsToolchain" = true; then
         toolchainFile=$( absolutePath $argument)
         nextArgIsToolchain=false
+        if [[ -f $toolchainFile ]]; then
+          mountArguments+="--mount type=bind,source=${toolchainFile},target=/home/userfiles/$(basename $toolchainFile) "
+        else
+          echo "No toolchain definition found"
+          exit 1
+        fi
       elif test "$nextArgIsInput" = true; then
         sourceFile=$( absolutePath $argument)
         nextArgIsInput=false
+        if [[ -f $sourceFile ]]; then
+          mountArguments+="--mount type=bind,source=${sourceFile},target=/home/userfiles/$(basename $sourceFile) "
+        else
+          echo "No source for input found"
+          exit 1
+        fi
       elif test "$nextArgIsSetting" = true; then
         settingFile=$( absolutePath $argument)
         nextArgIsSetting=false
+        if [[ -f "$settingFile" ]]; then
+          mountArguments+="--mount type=bind,source=${settingFile},target=/home/userfiles/$(basename $settingFile) "
+        else
+          echo "No settings found"
+          exit 1
+        fi
       elif test "$nextArgIsCsvDir" = true; then
         csvDir=$( absolutePath $argument)
         nextArgIsCsvDir=false
+        if [[ -d $csvDir ]]; then
+          mountArguments+="--mount type=bind,source=${csvDir},target=/home/csvfiles/ "
+        else
+          echo "No CSV directory to mount"
+          exit 1
+        fi
       else
         otherArguments+="${argument} "
       fi
   esac
 done
 
-# Start Ultimate inside docker container if all arguments are valid
-mountArguments="--mount type=bind,source=$(pwd)/logfiles,target=/home/logfiles "
-
-if [[ -f "$settingFile" ]]; then
-  mountArguments+="--mount type=bind,source=${settingFile},target=/home/userfiles/$(basename $settingFile) "
-else
-  echo "No settings found"
-fi
-
-if [[ -f $toolchainFile ]]; then
-  mountArguments+="--mount type=bind,source=${toolchainFile},target=/home/userfiles/$(basename $toolchainFile) "
-else
-  echo "No toolchain definition found"
-fi
-
-if [[ -f $sourceFile ]]; then
-  mountArguments+="--mount type=bind,source=${sourceFile},target=/home/userfiles/$(basename $sourceFile) "
-else
-  echo "No source for input found"
-fi
-
-if [[ -d $csvDir ]]; then
-  mountArguments+="--mount type=bind,source=${csvDir},target=/home/csvfiles/ "
-else
-  echo "No CSV directory to mount"
-fi
-
-mkdir -p ./logfiles
-
-# Build and execute container
+# Build and execute Ultimate inside a container
 docker build -f Dockerfile_UAWrapper -t ultimate-wrapper .
 docker run $mountArguments --env OTHER_ARGUMENTS="$otherArguments" --rm ultimate-wrapper #/bin/bash
